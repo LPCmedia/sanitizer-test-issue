a68a8e32b61181af7b685a413a5b951fd68282c6
pboudreau <patrick@lpcmedia.com>
2021-11-24 22:34:11 -0500

    ng-mocks-works

--- apps/fooo/src/app/app.component.spec.ts
+++ apps/fooo/src/app/app.component.spec.ts
@@ -1,31 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MockComponent } from 'ng-mocks';
+import { FooComponent } from './foo/foo.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      declarations: [AppComponent, MockComponent(FooComponent)],
+      providers: [
+      ]
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('with fixture detect changes', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+    fixture.detectChanges();
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fooo'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('fooo');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome to fooo!'
-    );
-  });
 });
--- apps/fooo/src/app/app.module.ts
+++ apps/fooo/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooComponent } from './foo/foo.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FooComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
